83d43895dee05e8f4f6aac1857da1b8f97fcabf9
Craig Lynagh <craiglynagh@gmail.com>
2017-07-17 21:24:38 +0100

    results page added and working

--- controller.rb
+++ controller.rb
@@ -11,6 +11,10 @@ get '/play' do
   erb(:play)
 end
 
+get '/result' do
+  erb(:result)
+end
+
 get '/play/:hand1/:hand2' do
   game = Game.new(params[:hand1], params[:hand2])
   @game_result = game.run
